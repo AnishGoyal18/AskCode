959a057b6e090d66183bc49be214211f6f83c70c
Anish Goyal <anishgoyal2020@gmail.com>
2023-09-04 19:04:58 +0530

    Remove loading while fetching questions

--- frontend/src/context/QuestionContext.js
+++ frontend/src/context/QuestionContext.js
@@ -1,19 +1,16 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import ReactLoading from 'react-loading';
 import axios from 'axios';
 
 const questionContext = createContext();
 
 export function QuestionContextProvider({ children }) {
     const [allQuestions, setAllQuestions] = useState([]);
-    const [pending, setPending] = useState(true);
 
     useEffect(() => {
-        async function fetchData() {
+        const fetchData = async () => {
             try {
                 const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/questions`);
                 setAllQuestions(res.data.reverse());
-                setPending(false);
             } catch (error) {
                 console.error(error);
             }
@@ -21,14 +18,6 @@ export function QuestionContextProvider({ children }) {
 
         fetchData();
     }, [allQuestions]);
-
-    if (pending) {
-        return (
-            <div className='flex h-screen justify-center items-center'>
-                <ReactLoading type='bars' color='gray' />
-            </div>
-        )
-    }
 
     return (
         <questionContext.Provider value={{ allQuestions, setAllQuestions }}>
